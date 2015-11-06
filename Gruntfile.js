42c177232ab9defb16b606f4dbf41a26dc13d906
ma-nakamura <nakamura40666@sanix.jp>
2015-11-06 10:55:08 +0900

    s3 upload function added

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,12 +11,9 @@ module.exports = function(grunt) {
       secret: '<%= s3auth.secret %>',
       region: 'ap-northeast-1',
       bucket: 'sanix-data-analysis',
-      access: 'public-read'
       },
-      ore_no_site: {
-        upload: [
-        { src: 'deploy/**/*', dest: 'root' }
-        ]
+      sanixEyes: {
+        upload: [ { src: 'deploy/index.html', dest: 'test/index.html' } ]
       }
     },
     watch : {
@@ -24,63 +21,62 @@ module.exports = function(grunt) {
         files:['**'],
         tasks:['deploy']
       }
-      },
-      coffee: {
-        deploy:{
-          files:[{
-            expand:true,
-            cwd:'coffee',
-            src:[
-            '*.coffee'
-            ],
-            dest:'deploy',
-            ext:'.js'
-            }]
-          }
-          },
-          jade: {
-            options:{
-              data:jadeDataFunc(),
+    },
+    coffee: {
+      deploy:{
+        files:[{
+          expand:true,
+          cwd:'coffee',
+          src:[
+          '*.coffee'
+          ],
+          dest:'deploy',
+          ext:'.js'
+        }]
+      }
+    },
+    jade: {
+      options:{
+        data:jadeDataFunc(),
         pretty:true // コードを整形出力するか
-        },
-        deploy:{
-          files:[{
-            expand:true,
-            cwd:'jade',
-            src:[
-            '*.jade'
-            ],
-            dest:'deploy',
-            ext:'.html'
-            }],
-          }
-        }
-        });
-function jadeDataFunc() {
-  return function(dest, srcAr) {
-    var _ = grunt.util._,
-    regDest = /\/([A-Za-z_0-9-]+?)\.html/,
-    destName = dest.match(regDest)[1],
-    data;
-    try {
-      data = grunt.file.readJSON("json/_common.json");
+      },
+      deploy:{
+        files:[{
+          expand:true,
+          cwd:'jade',
+          src:[
+          '*.jade'
+          ],
+          dest:'deploy',
+          ext:'.html'
+        }],
+      }
+    }
+  });
+
+  function jadeDataFunc() {
+    return function(dest, srcAr) {
+      var _ = grunt.util._,
+      regDest = /\/([A-Za-z_0-9-]+?)\.html/,
+      destName = dest.match(regDest)[1],
+      data;
+      try {
+        data = grunt.file.readJSON("json/_common.json");
       } catch(e) {
         data = {};
       }
       return _.extend({
         page:destName
-        }, data);
+      }, data);
     };
   }
 
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-jade');
-  grunt.loadNpmTasks('grunt-contrib-coffee');
-  grunt.registerTask('deploy', ['jade', 'coffee']);
   grunt.loadNpmTasks('grunt-s3');
-  grunt.registerTask('upload', 's3');
+  grunt.registerTask('deploy', ['jade', 's3']);
 
   grunt.loadTasks('tasks');
 
-  grunt.registerTask('default', ['deploy', 'coffee']);
+  grunt.registerTask('default', ['deploy']);
 };
